0713a195f14de1fce64cd115af9842dfbdab0179
zl <zl@ubuntu-zl.(none)>
2017-09-05 20:29:12 +0800

    添加长跳转测试程序

--- module/jmp_buf.cpp
+++ module/jmp_buf.cpp
@@ -1,41 +1,55 @@
+#include <assert.h>
+#include <setjmp.h>
 #include <stdio.h>
-#include <ext/hash_map>
-#include <string>
-//#include <map>
+#include <stdlib.h>
 
-using namespace __gnu_cxx;
-using namespace std;
+/*
+跨函数跳转（长跳转）：场景则常见于多进程的模块化处理系统中，用于回到特定（初始）状态。
 
-typedef hash_map<string, int> HASHMAP;
-//typedef map<string, int> HASHMAP;
+我们假设，一个手机维修店有客户经理和维修工程师两个人。客户经理只是负责和客户沟通、订合同、收货、收钱，具体的维修工作由后者处理。工程师接到一个维修任务，
+打开手机外壳后一看，就说三个字“没得搞”，然后外壳也不拧上，就直接扔掉，休息休息去忙下一个机器的维修。至于客户经理怎么和客户沟通，那不是工程师的事，最多拍个照片以做证明。
 
-void Director(const HASHMAP &hashmapTest)
+对于模块化的系统，我们假设也有两个进程，一个进程A负责计算，一个进程B负责通过socket或其他方式获取数据及发布计算结果。
+两个进程之间协作，通过任务的方式推动而不是函数的方式调用。进程B，就如同维修工程师，当给入的数据无法计算或者计算导致错误时，
+则直接长跳转到最初的状态，继续等待下一个任务。当然人性化一点，多少告知一下进程A，方便它给客户端一个交代。
+*/
+
+static jmp_buf jmp;
+
+float a(float i, float j)
 {
-    HASHMAP::const_iterator ite;
-
-    for(ite = hashmapTest.begin(); ite != hashmapTest.end(); ite++)
+    if(0 == j)
     {
-        printf("----key = %s, value=%d\n", ite->first.c_str(), ite->second);
+        printf("called longjimp......\n");
+
+        longjmp(jmp, 2);
     }
 
+    return i / j;
 }
 
-int main()
+float b(float x1, float x2)
 {
-    HASHMAP hashTest;
-    string str1 = "0";
-    string str2 = "1";
-    string str3 = "2";
+    return a(x1+x2, x1-x2);
+}
 
+int main(int argc, char* argv[])
+{
+    float x1, x2;
 
-    int i = 0;
-    hashTest.insert(make_pair(str1, i));
-    hashTest.insert(make_pair(str2, i+1));
-    hashTest.insert(make_pair(str3, i+2));
+    assert(argc > 2);
 
+    x1 = atof(argv[1]);
+    x2 = atof(argv[2]);
 
-    Director(hashTest);
+    if(0 == setjmp(jmp))
+    {
+        printf("(%f + %f) / (%f - %f) = %f\n", x1, x2, x1, x2, b(x1, x2));
+    }
+    else
+    {
+        printf("(%f - %f) error!\n", x1, x2);
+    }
 
     return 0;
-
 }
